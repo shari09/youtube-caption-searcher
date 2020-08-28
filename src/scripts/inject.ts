c238367385e5d6108990cdb8aa2a5b443c90fb86
shari09 <shari09cxxcr@gmail.com>
2020-08-28 01:02:02 -0400

    able to load caption tracks after clicking into videos from playlist/recommended

--- src/scripts/inject.ts
+++ src/scripts/inject.ts
@@ -1,25 +1,103 @@
-import 'chrome-extension-async';
-export const getActiveTabId = async (): Promise<number> => {
-  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
-  console.log(tabs);
-  if (!tabs) throw new Error('invalid active tab');
-  if (!tabs[0].id) throw new Error('active tab id not found');
-  return tabs[0].id;
+
+// export const getActiveTabId = async (): Promise<number> => {
+//   const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+//   console.log(tabs);
+//   if (!tabs) throw new Error('invalid active tab');
+//   if (!tabs[0].id) throw new Error('active tab id not found');
+//   return tabs[0].id;
+// };
+
+// const getCurUrl = async (): Promise<string> => {
+//   const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+//   if (!tabs[0] || !tabs[0].url) throw new Error('invalid active tab');
+//   return tabs[0].url;
+// };
+
+// const getCurUrl = (): Promise<string> => {
+//   return new Promise((resolve, reject) => {
+//     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+//       console.log(tabs);
+//       if (!tabs[0] || !tabs[0].url) reject('invalid active tab');
+//       console.log(tabs[0].url);
+//       resolve(tabs[0].url);
+//     });
+//   });
+// };
+
+// const urlDecode = (encoded: string) => {
+//   return '{' + decodeURIComponent(encoded.replace(/\+/g, ' ')) + '}';
+// };
+
+// const sendCaptionTracks = () => {
+  // const captionTrackRegex = /"captionTracks":\[.*?\]/g;
+  // const captionTrackMatch = captionTrackRegex.exec(
+  //   //@ts-ignore
+  //   ytplayer.config.args.player_response,
+  // );
+//   const captionTracks = captionTrackMatch
+//     ? JSON.parse(urlDecode(captionTrackMatch[0]))
+//     : null;
+
+//   document.dispatchEvent(
+//     new CustomEvent('captionTracks', {
+//       detail: captionTracks,
+//     }),
+//   );
+//   console.log('sent new caption track');
+//   console.log(captionTracks);
+// };
+console.log('whaaaaaaaa');
+
+/**
+ * getting the request url
+ * @param videoId the video that you hope to get captions for
+ */
+const getReqUrl = (videoId: string) => {
+  const origin = window.location.origin;
+  const pathname = window.location.pathname;
+  const xmhReqParam = 'pbj=1';
+  //this is so sketchy i'll use the url module later
+  return origin + pathname + '?'+ videoId + '&' + xmhReqParam;
 };
 
-const urlDecode = (encoded: string) => {
-  return '{' + decodeURIComponent(encoded.replace(/\+/g, ' ')) + '}';
-};
-
-const sendCaptionTracks = () => {
-  const captionTrackRegex = /"captionTracks":\[.*?\]/g;
-  const captionTrackMatch = captionTrackRegex.exec(
-    //@ts-ignore
-    ytplayer.config.args.player_response,
-  );
-  const captionTracks = captionTrackMatch
-    ? JSON.parse(urlDecode(captionTrackMatch[0]))
-    : null;
+const sendCaptionTracks = async(videoId: string) => {
+  
+  //get the newly loaded data
+  //because somehow DOM doesn't update on a new video click, so this is how you're gonna get it
+  //these auth fields are what the api needs otherwise it returns {reload: true}
+  //@ts-ignore
+  const token = window.yt.config_.ID_TOKEN;
+  //@ts-ignore
+  const clientVersion = window.yt.config_.INNERTUBE_CONTEXT_CLIENT_VERSION;
+  //@ts-ignore
+  const clientName = window.yt.config_.INNERTUBE_CONTEXT_CLIENT_NAME;
+  const reqUrl = getReqUrl(videoId);
+  console.log(reqUrl);
+  const vidRawData = await fetch(reqUrl, {
+    headers: {
+      'x-youtube-client-name': clientName,
+      'x-youtube-client-version': clientVersion,
+      'x-youtube-identity-token': token,
+    },
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  });
+  const response = await vidRawData.json();
+  //@ts-ignore
+  let captionTracks;
+  if (!response[2].playerResponse.captions) {
+    captionTracks = null;
+  } else {
+    captionTracks = {
+      captionTracks: response[2]
+                    .playerResponse
+                    .captions
+                    .playerCaptionsTracklistRenderer
+                    .captionTracks
+    };
+  }
+  
 
   document.dispatchEvent(
     new CustomEvent('captionTracks', {
@@ -31,13 +109,8 @@ const sendCaptionTracks = () => {
 };
 
 let videoLinks: Element[] = [];
+let eventListeners: (() => void)[] = [];
 
-const removeListeners = () => {
-  videoLinks.forEach((element) => {
-    element.removeEventListener('click', clickEvent);
-  });
-  videoLinks = [];
-};
 
 const addListeners = () => {
   const clickableElements = Array.from(
@@ -51,15 +124,33 @@ const addListeners = () => {
     return true;
   });
 
-  videoLinks.forEach((element) => {
-    element.addEventListener('click', clickEvent);
+  videoLinks.forEach((element, index) => {
+    const listener = () => {
+      const href = videoLinks[index].getAttribute('href');
+      const videoIdMatch = /(v=[^!]+)&/.exec(href);
+      if (!videoIdMatch) throw new Error('cannot find video id from element');
+
+      removeListeners();
+      sendCaptionTracks(videoIdMatch[1]);
+      addListeners();
+    };
+    //each one is a different listener corresponding to a different element
+    eventListeners.push(listener);
+    element.addEventListener('click', eventListeners[index]);
   });
 };
 
-const clickEvent = () => {
-  removeListeners();
-  addListeners();
-  sendCaptionTracks();
+const removeListeners = () => {
+  videoLinks.forEach((element, index) => {
+    element.removeEventListener('click', eventListeners[index]);
+  });
+  videoLinks = [];
+  eventListeners = [];
 };
 
-clickEvent();
+
+const videoIdMatch = /(v=[^!]+)&/.exec(window.location.search);
+if (!videoIdMatch) throw new Error('cannot find video id from path');
+//initially get the videoId from url path
+sendCaptionTracks(videoIdMatch[1]);
+addListeners();
